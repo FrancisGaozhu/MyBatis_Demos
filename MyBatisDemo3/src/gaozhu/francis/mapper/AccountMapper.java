f5a5b2eeb37a148f2a58ea43a54c347f0c769748
FrancisGaozhu <francisgaozhu@foxmail.com>
2024-01-06 15:57:03 +0800

    完善注释

--- MyBatisDemo3/src/gaozhu/francis/mapper/AccountMapper.java
+++ MyBatisDemo3/src/gaozhu/francis/mapper/AccountMapper.java
@@ -16,13 +16,32 @@ public interface AccountMapper {
 
     /**
      * 根据ID进行查询
+     * 查询结果为一条数据，对应一个对象，因此我们返回值直接定义为对应对象的所属类类型即可。
+     *
      * @param id 账户ID
      * @return 数据结果模型
      */
     Account selectById(@Param("id") Integer id);
 
     /**
+     * 获取所有的数据
+     * 获取多条数据的时候，数据结果为多条，因此需要封装为多个对象，则使用List集合就OK。
+     *
+     * @return 结果集合
+     */
+    List<Account> selectAll();
+
+    /**
+     * 获取记录数目
+     * 结果内容为一个标量值。
+     *
+     * @return 记录数
+     */
+    int selectCount();
+
+    /**
      * 根据ID进行数据查询家结果为map
+     * 结果为1条数据，要求封装为Map形式。
      * @param id 账户ID
      * @return 结果封装为Map
      */
@@ -30,29 +49,16 @@ public interface AccountMapper {
 
     /**
      * 查询多条数据，结果为Map
+     * 那么就再套一层List即可
      * @return 数据查询结果
      */
     List<Map<String, Object>> selectAllMap();
 
     /**
      * 查询多条数据，结果为Map
-     * 指定id为key
+     * 指定key采取的字段值，则使用下面的注解即可。
      * @return 结果集合
      */
     @MapKey("a_id")
     Map<String, Object> selectAllMap2();
-
-    /**
-     * 获取所有的数据
-     * @return 结果集合
-     */
-    List<Account> selectAll();
-
-    /**
-     * 获取记录数目
-     * @return 记录数
-     */
-    int selectCount();
-
-
 }
